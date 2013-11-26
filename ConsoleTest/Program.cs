d8a3c1f762413dcd384f9e82d3de60bee1bb418f
loogn <loogn_0707@126.com>
2013-11-26 16:34:49 +0800

    ok

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -5,14 +5,41 @@ using System.Diagnostics;
 using System.Data.Common;
 using System.Collections.Generic;
 using System.Linq;
-
+using MongoDB.Driver;
+using System.Text;
+using Loogn.Common;
+using MongoDB.Bson;
 namespace ConsoleTest
 {
     class Program
     {
+        static string mgConn = "server=192.168.18.187:27017;database=papillon;username=pp;password=pp;connect=direct;maxPoolSize=200;connectTimeout=1m;";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+
+            var dt = DateTime.Parse("2013-11-26 03:00:00");
+
+            Console.WriteLine(dt.ToUniversalTime().ToString("yyyyMMdd"));
+
+            Console.WriteLine(dt.ToString("yyyyMMdd"));
+
+
+            return;
+
+            List<BsonDocument> list = new List<BsonDocument>(280000);
+            foreach (var line in File.ReadAllLines(@"e:\treedict.txt"))
+            {
+                list.Add(new BsonDocument { { "_id", line } });
+            }
+
+
+            MongoClient mc = new MongoClient(mgConn);
+
+            var db = mc.GetServer().GetDatabase("papillon");
+            var coll = db.GetCollection("dict");
+            coll.InsertBatch(list);
+            
+            //  "C#":[{1,1}]
         }
     }
 }
