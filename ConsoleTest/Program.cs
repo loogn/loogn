0d8a9bc0857044d5f9873e6c91a6111f02414e28
loogn <loogn_0707@126.com>
2013-12-06 17:36:00 +0800

    ok

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -9,19 +9,64 @@ using MongoDB.Driver;
 using System.Text;
 using Loogn.Common;
 using MongoDB.Bson;
+using Loogn.Common.Aspect;
 namespace ConsoleTest
 {
+    public class Log : IAspectAdvice
+    {
+        public void Before(object target, System.Reflection.MethodInfo mi, params object[] args)
+        {
+            Console.WriteLine("Before");
+        }
+
+        public void After(object target, System.Reflection.MethodInfo mi, params object[] args)
+        {
+            Console.WriteLine("After");
+        }
+
+        public void Throw(object target, System.Reflection.MethodInfo mi, params object[] args)
+        {
+            Console.WriteLine("Throw");
+        }
+    }
+
+    public static class TT
+    {
+        public static void After(this Action act, params IAspectAdvice[] advices)
+        {
+            act();
+            foreach (var ad in advices)
+            {
+                ad.After(act.Target, act.Method);
+            }
+        }
+    }
+
     class Program
     {
         static string mgConn = "server=192.168.18.187:27017;database=papillon;username=pp;password=pp;connect=direct;maxPoolSize=200;connectTimeout=1m;";
 
+        void F()
+        {
+            Console.WriteLine("Do F");
+        }
+
+
         static void Main(string[] args)
         {
-            MongoClient mc = new MongoClient(mgConn);
-            var db= mc.GetServer().GetDatabase("papillon");
-            MongoDB.Driver.GridFS.MongoGridFS fs = new MongoDB.Driver.GridFS.MongoGridFS(db);
+            Program p = new Program();
 
-            fs.Delete("abc.txt");
+            var s = "sdfsdf".AsInt32();
+            if (s != null)
+            {
+                Console.WriteLine(s.Value);
+            }
+            else {
+                Console.WriteLine("null");
+            }
+
+            //AspectInvoker.AfterAction(F, new Log());
+
             //var stream = fs.Create("abc.txt", new MongoDB.Driver.GridFS.MongoGridFSCreateOptions()
             //{
             //    Aliases = new string[] { "ab", "bc" },
