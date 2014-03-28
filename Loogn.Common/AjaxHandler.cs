3131f326767750d0ecfb8b694cd1bb544c3f3729
loogn <loogn_0707@126.com>
2014-03-28 14:50:25 +0800

    ok

---
+++ Loogn.Common/AjaxHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Loogn.Common
+{
+    /// <summary>
+    /// 用户处理ajax请求
+    /// </summary>
+    /// <param name="state"></param>
+    /// <returns></returns>
+    public delegate string AjaxHandler(object state = null);
+}
