0036b3ed7f28aab82722cd9a76434c77f282e996
loogn <loogn_0707@126.com>
2014-04-08 07:54:17 +0800

    ok

--- Loogn.Common/AjaxHandler.cs
+++ Loogn.Common/AjaxHandler.cs
@@ -11,4 +11,22 @@ namespace Loogn.Common
     /// <param name="state"></param>
     /// <returns></returns>
     public delegate string AjaxHandler(object state = null);
+
+    public class AjaxHandlerHelper
+    {
+        public static Dictionary<string, AjaxHandler> BuildHandlers(Type type)
+        {
+            Dictionary<string, AjaxHandler> hs = new Dictionary<string, AjaxHandler>();
+            var msArr = type.GetMethods(System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+            for (int i = 0; i < msArr.Length; i++)
+            {
+                var ms = msArr[i];
+                hs.Add(ms.Name.ToLower(), (obj) =>
+                {
+                    return ms.Invoke(null, new object[] { obj }).ToString();
+                });
+            }
+            return hs;
+        }
+    }
 }
